88c1368bd9084b9358d488e33aa4f2a90c671493
Ubuntu <ubuntu@clojure.lxd>
2024-03-16 19:05:24 +0000

    *

--- src/7-components-rendering/app/ui.cljs
+++ src/7-components-rendering/app/ui.cljs
@@ -1,30 +1,43 @@
 (ns app.ui
   (:require
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom]))
+   [com.fulcrologic.fulcro.dom :as dom]
+   [com.fulcrologic.fulcro.mutations :as m]
+   [app.api :as api]))
 
-(defsc Person [this {:person/keys [name surname]}]
+(defsc Person [this {:person/keys [name surname]
+                     :ui/keys [checked?]}]
   {:query [:person/id :person/name
-           :person/surname :person/age]
+           :person/surname :person/age :ui/checked?]
    :ident :person/id}
   (dom/li
-   (dom/input {:type "checkbox"})
+   (dom/input {:type "checkbox"
+               :checked (true? checked?)
+               :onChange #(m/toggle! this :ui/checked?)})
    (dom/label (str name " " surname))))
 
 (def ui-person (comp/factory Person
                              {:keyfn :person/id}))
 
 (defsc PersonList [this {:list/keys [items]}]
-  (dom/ul
-   (dom/li
-    (dom/input {:type "checkbox"})
-    (dom/label "select all"))
-   (mapv ui-person items)))
+  (let [all-checked? (if (empty? items)
+                       false
+                       (every? :ui/checked? items))]
+    (dom/ul
+     (dom/li
+      (dom/input {:type "checkbox"
+                  :checked all-checked?
+                  :onChange #(if all-checked?
+                               (comp/transact! this [(api/uncheck-all {:list-id :root/people})])
+                               (comp/transact! this [(api/check-all {:list-id :root/people})]))})
+      (dom/label "select all"))
+     (mapv ui-person items))))
 
 (def ui-person-list (comp/factory PersonList))
 
-(defsc Root [this data]
-  {:query [{:root/people (comp/get-query Person)}]}
+(defsc Root [_ data]
+  {:query [{:root/people (comp/get-query Person)}]
+   :initial-state {}}
   (dom/div
    (ui-person-list {:list/items (:root/people data)})))
 
